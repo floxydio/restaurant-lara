cff3ac48c423a3e5f350ca5f3430b3e68e1ac341
wintonalmundarisna <wintonalmundarisna@gmail.com>
2023-10-23 14:19:24 +0700

    Adding Restaurant Detail and Staff Call

--- routes/api.php
+++ routes/api.php
@@ -125,7 +125,7 @@ Route::delete("/v1/complain-delete/{id}", function (Request $request, $id) {
 
 // ? ==== Table Order
 
-Route::get("/v1/table_order", function () {
+Route::get("/v1/table-order", function () {
     $dbTable = DB::table("table_order")->get();
     if ($dbTable == null) {
         return response()->json([
@@ -143,7 +143,7 @@ Route::get("/v1/table_order", function () {
     }
 });
 
-Route::post("/v1/table_order/create", function (Request $request) {
+Route::post("/v1/table-order/create", function (Request $request) {
     $dbTable = DB::table("table_order")->insert([
         "name" => $request->input("name"),
     ]);
@@ -163,7 +163,7 @@ Route::post("/v1/table_order/create", function (Request $request) {
     }
 });
 
-Route::get("/v1/table_order/{id}", function (Request $request, $id) {
+Route::get("/v1/table-order/{id}", function (Request $request, $id) {
     $dbTable = DB::table("table_order")->where("id", $id)->get();
     if ($dbTable == null) {
         return response()->json([
@@ -224,7 +224,99 @@ Route::delete("/v1/table-delete/{id}", function (Request $request, $id) {
 
 // ! ==== Staff Call
 
+Route::get("/v1/staff-call", function () {
+    $dbStaff = DB::table("staff_call")->get();
+    if ($dbStaff == null) {
+        return response()->json([
+            "status" => 400,
+            "error" => true,
+            "message" => "Something Went Wrong"
+        ], 400);
+    } else {
+        return response()->json([
+            "status" => 200,
+            "error" => false,
+            "message" => "Successfully Get Message",
+            "data" => $dbStaff
+        ], 200);
+    }
+});
 
+Route::post("/v1/staff-call/create", function (Request $request) {
+    $dbStaff = DB::table("staff_call")->insert([
+        "table_id" => $request->input("table_id"),
+        "message" => $request->input("message"),
+    ]);
+
+    if ($dbStaff == null) {
+        return response()->json([
+            "status" => 400,
+            "error" => true,
+            "message" => "Something Went Wrong"
+        ], 400);
+    } else {
+        return response()->json([
+            "status" => 201,
+            "error" => false,
+            "message" => "Successfully Create Message to Staff",
+        ], 201);
+    }
+});
+
+Route::get("/v1/staff-call/{id}", function (Request $request, $id) {
+    $dbStaff = DB::table("staff_call")->where("id", $id)->get();
+    if ($dbStaff == null) {
+        return response()->json([
+            "status" => 400,
+            "error" => true,
+            "message" => "Something Went Wrong"
+        ], 400);
+    } else {
+        return response()->json([
+            "status" => 200,
+            "error" => false,
+            "message" => "Successfully Get Detail Message",
+            "data" => $dbStaff
+        ], 200);
+    }
+});
+
+Route::put("/v1/staff-call/{id}", function (Request $request, $id) {
+    $dbTable = DB::table("staff_call")->where("id", $id)->update([
+        "table_id" => $request->input("table_id"),
+        "message" => $request->input("message"),
+    ]);
+    if ($dbTable == null) {
+        return response()->json([
+            "status" => 400,
+            "error" => true,
+            "message" => "Something Went Wrong"
+        ], 400);
+    } else {
+        return response()->json([
+            "status" => 200,
+            "error" => false,
+            "message" => "Successfully Update Message to Staff",
+        ], 200);
+    }
+});
+
+Route::delete("/v1/staff-delete/{id}", function (Request $request, $id) {
+    $dbTable = DB::table("staff_call")->delete($id);
+    if ($dbTable == null) {
+        return response()->json([
+            "status" => 400,
+            "error" => true,
+            "message" => "Something Went Wrong"
+        ], 400);
+    } else {
+        return response()->json([
+            "status" => 200,
+            "error" => false,
+            "message" => "Successfully Delete Message to Staff",
+        ], 200);
+    }
+});
 
 // ! ==== Staff Call
 
@@ -250,9 +342,14 @@ Route::get("/v1/restaurant", function () {
     }
 });
 
-Route::post("/v1/table_order/create", function (Request $request) {
-    $dbResto = DB::table("table_order")->insert([
-        "name" => $request->input("name"),
+Route::post("/v1/restaurant/create", function (Request $request) {
+    $dbResto = DB::table("restaurant_detail")->insert([
+        "setting_name" => $request->input("setting_name"),
+        "open_hour" => $request->input("open_hour"),
+        "closed_hour" => $request->input("closed_hour"),
+        "location" => $request->input("location"),
+        "description" => $request->input("description"),
+        "logo" => $request->input("logo"),
     ]);
 
     if ($dbResto == null) {
@@ -265,14 +362,14 @@ Route::post("/v1/table_order/create", function (Request $request) {
         return response()->json([
             "status" => 201,
             "error" => false,
-            "message" => "Successfully Create Table",
+            "message" => "Successfully Create Restaurant Detail",
         ], 201);
     }
 });
 
-Route::get("/v1/table_order/{id}", function (Request $request, $id) {
-    $dbTable = DB::table("table_order")->where("id", $id)->get();
-    if ($dbTable == null) {
+Route::get("/v1/restaurant/{setting_name}", function (Request $request, $setting_name) {
+    $dbResto = DB::table("restaurant_detail")->where("setting_name", $setting_name)->get();
+    if ($dbResto == null) {
         return response()->json([
             "status" => 400,
             "error" => true,
@@ -282,16 +379,21 @@ Route::get("/v1/table_order/{id}", function (Request $request, $id) {
         return response()->json([
             "status" => 200,
             "error" => false,
-            "message" => "Successfully Get Detail Table",
-            "data" => $dbTable
+            "message" => "Successfully Get Restaurant Detail",
+            "data" => $dbResto
         ], 200);
     }
 });
 
 
-Route::put("/v1/table-update/{id}", function (Request $request, $id) {
-    $dbTable = DB::table("table_order")->where("id", $id)->update([
-        "name" => $request->input("name"),
+Route::put("/v1/restaurant-update/{setting_name}", function (Request $request, $setting_name) {
+    $dbTable = DB::table("restaurant_detail")->where("setting_name", $setting_name)->update([
+        "setting_name" => $request->input("setting_name"),
+        "open_hour" => $request->input("open_hour"),
+        "closed_hour" => $request->input("closed_hour"),
+        "location" => $request->input("location"),
+        "description" => $request->input("description"),
+        "logo" => $request->input("logo"),
     ]);
     if ($dbTable == null) {
         return response()->json([
@@ -303,14 +405,14 @@ Route::put("/v1/table-update/{id}", function (Request $request, $id) {
         return response()->json([
             "status" => 200,
             "error" => false,
-            "message" => "Successfully Update Table Order",
+            "message" => "Successfully Update Restaurant Detail",
         ], 200);
     }
 });
 
-Route::delete("/v1/table-delete/{id}", function (Request $request, $id) {
-    $dbTable = DB::table("table_order")->delete($id);
-    if ($dbTable == null) {
+Route::delete("/v1/restaurant-delete/{setting_name}", function (Request $request, $setting_name) {
+    $dbResto = DB::table("restaurant_detail")->delete($setting_name);
+    if ($dbResto == null) {
         return response()->json([
             "status" => 400,
             "error" => true,
